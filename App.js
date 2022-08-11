070e459a365af8d275147ea35f16ca14d5c2cda7
tejas-gulhane <103639207+tejas-gulhane@users.noreply.github.com>
2022-08-11 07:50:18 +0530

    M4-TASK _11

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { useState, useCallback, useMemo } from 'react';
+import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
+
+function App() {
+  const [listTitle, setListTitle] = useState('My List');
+  const [desending, setDesending] = useState(false);
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
+
+  const desendingOrderHandler = useCallback(() => {
+    setDesending(!desending);
+  }, [desending])
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
+  return (
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} desending={desending} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <Button onClick={desendingOrderHandler}>{!desending ? 'Change To Desending Order': 'Change To Assending Order'}</Button>
+    </div>
+  );
+}
+export default App;
